a1df074ac15a34d54ffc6a08749226918a07baf6
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2012-02-08 07:51:40 +0900

    refactor queue.

--- test/queue.js
+++ test/queue.js
@@ -1,45 +1,25 @@
-var queue = require('../lib/nue').queue;
+var nue = require('../lib/nue');
+var flow = nue.flow;
 var assert = require('assert');
 
 describe('queue', function() {
-  it('should chain queued tasks', function (done) {
-    var q = queue(
-      function (i) {
-        if (this.isFirst) {
-          this.data = [];
+  it('should be used in "flow"', function (done) {
+    flow(
+      function () {
+        var q = this.queue(function (i) {
+          this.next(i * 2);
+        });
+        for (var i = 0; i < 10; i++) {
+          q.push(i);
         }
-        this.data.push(i);
-        if (this.isLast) {
-          assert.deepEqual(this.data, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-          done();
-        }
-        this.next();
-      }
-    );
-    for (var i = 0; i < 10; i++) {
-      q.push(i);
-    }
-    q.complete();
-  });
-  it('should accept arguments from the previous task', function (done) {
-    var q = queue(
-      function (i) {
-        this.next(i * 2);
+        q.complete();
       },
-      function (err, results) {
-        assert.strictEqual(null, err);
-        assert.strictEqual(results.length, 5);
-        assert.strictEqual(results[0], 0);
-        assert.strictEqual(results[1], 2);
-        assert.strictEqual(results[2], 4);
-        assert.strictEqual(results[3], 6);
-        assert.strictEqual(results[4], 8);
+      function (results) {
+        assert.ok(!this.err);
+        assert.deepEqual(results, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
         done();
       }
-    );
-    for (var i = 0; i < 5; i++) {
-      q.push(i);
-    }
-    q.complete();
+    )();
   });
+
 });
