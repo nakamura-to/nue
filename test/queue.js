0dcb4e944dd117ef057c2b267d648a1f9eae15c6
nakamura-to <toshihiro.nakamura@gmail.com>
2012-02-08 10:40:56 +0900

    refactor queue.

--- test/queue.js
+++ test/queue.js
@@ -1,25 +1,234 @@
 var nue = require('../lib/nue');
 var flow = nue.flow;
+var each = nue.each;
+var parallel = nue.parallel;
+var parallelEach = nue.parallelEach;
 var assert = require('assert');
 
 describe('queue', function() {
-  it('should be used in "flow"', function (done) {
-    flow(
-      function () {
-        var q = this.queue(function (i) {
-          this.next(i * 2);
-        });
-        for (var i = 0; i < 10; i++) {
-          q.push(i);
+  // FLOW
+  describe('flow', function () {
+    it('should chain with "next"', function (done) {
+      flow(
+        function () {
+          var q = this.queue(function (i) {
+            this.next(i * 2);
+          });
+          for (var i = 0; i < 10; i++) {
+            q.push(i);
+          }
+          q.complete();
+        },
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
+          done();
         }
-        q.complete();
-      },
-      function (results) {
-        assert.ok(!this.err);
-        assert.deepEqual(results, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
-        done();
-      }
-    )();
+      )();
+    });
+
+    it('should chain with "callback"', function (done) {
+      flow(
+        function () {
+          var q = this.queue(function (i) {
+            this.callback(null, i * 2);
+          });
+          for (var i = 0; i < 10; i++) {
+            q.push(i);
+          }
+          q.complete();
+        },
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
+          done();
+        }
+      )();
+    });
+
+    it('should exit with "end" in "flow"', function (done) {
+      flow(
+        function () {
+          var q = this.queue(function (i) {
+            this.end('ERROR');
+          });
+          for (var i = 0; i < 10; i++) {
+            q.push(i);
+          }
+          q.complete();
+        },
+        function (results) {
+          assert.strictEqual(this.err, 'ERROR');
+          assert.ok(!results);
+          done();
+        }
+      )();
+    });
+
+    it('should exit with "callback"', function (done) {
+      flow(
+        function () {
+          var q = this.queue(function (i) {
+            this.callback('ERROR');
+          });
+          for (var i = 0; i < 10; i++) {
+            q.push(i);
+          }
+          q.complete();
+        },
+        function (results) {
+          assert.strictEqual(this.err, 'ERROR');
+          assert.ok(!results);
+          done();
+        }
+      )();
+    });
+  });
+
+  // EACH
+  describe('each', function () {
+    it('should chain with "next"', function (done) {
+      flow(
+        each(function (len) {
+          var q = this.queue(function (i) {
+            this.next(i * 2);
+          });
+          for (var i = 0; i < len; i++) {
+            q.push(i);
+          }
+          q.complete();
+        }),
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [ [ 0 ], [ 0, 2 ], [ 0, 2, 4 ] ]);
+          done();
+        }
+      )(1, 2, 3);
+    });
+
+    it('should chain with "callback"', function (done) {
+      flow(
+        each(function (len) {
+          var q = this.queue(function (i) {
+            this.callback(null, i * 2);
+          });
+          for (var i = 0; i < len; i++) {
+            q.push(i);
+          }
+          q.complete();
+        }),
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [ [ 0 ], [ 0, 2 ], [ 0, 2, 4 ] ]);
+          done();
+        }
+      )(1, 2, 3);
+    });
+
+  });
+
+  // PARALLEL
+  describe('parallel', function () {
+    it('should chain with "next"', function (done) {
+      flow(
+        parallel(
+          function (len) {
+            var q = this.queue(function (i) {
+              this.callback(null, i * 2);
+            });
+            for (var i = 0; i < len; i++) {
+              q.push(i);
+            }
+            q.complete();
+          },
+          function (len) {
+            var q = this.queue(function (i) {
+              this.callback(null, i * 2);
+            });
+            for (var i = 0; i < len; i++) {
+              q.push(i);
+            }
+            q.complete();
+          }
+        ),
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [ [ 0, 2 ], [ 0, 2, 4 ] ]);
+          done();
+        }
+      )(2, 3);
+    });
+
+    it('should chain with "callback"', function (done) {
+      flow(
+        parallel(
+          function (len) {
+            var q = this.queue(function (i) {
+              this.callback(null, i * 2);
+            });
+            for (var i = 0; i < len; i++) {
+              q.push(i);
+            }
+            q.complete();
+          },
+          function (len) {
+            var q = this.queue(function (i) {
+              this.callback(null, i * 2);
+            });
+            for (var i = 0; i < len; i++) {
+              q.push(i);
+            }
+            q.complete();
+          }
+        ),
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [ [ 0, 2 ], [ 0, 2, 4 ] ]);
+          done();
+        }
+      )(2, 3);
+    });
+  });
+
+  // PARALLELEACH
+  describe('parallelEach', function () {
+    it('should chain with "next"', function (done) {
+      flow(
+        parallelEach(function (len) {
+          var q = this.queue(function (i) {
+            this.next(i * 2);
+          });
+          for (var i = 0; i < len; i++) {
+            q.push(i);
+          }
+          q.complete();
+        }),
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [ [ 0 ], [ 0, 2 ], [ 0, 2, 4 ] ]);
+          done();
+        }
+      )(1, 2, 3);
+    });
+
+    it('should chain with "callback"', function (done) {
+      flow(
+        parallelEach(function (len) {
+          var q = this.queue(function (i) {
+            this.callback(null, i * 2);
+          });
+          for (var i = 0; i < len; i++) {
+            q.push(i);
+          }
+          q.complete();
+        }),
+        function (results) {
+          assert.ok(!this.err);
+          assert.deepEqual(results, [ [ 0 ], [ 0, 2 ], [ 0, 2, 4 ] ]);
+          done();
+        }
+      )(1, 2, 3);
+    });
   });
 
 });
