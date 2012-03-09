376f0b32045e969a4fb96dfae196e3e00efc019c
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2012-03-10 08:30:10 +0900

    remove `history` property from step context.

--- examples/parallel.js
+++ examples/parallel.js
@@ -2,23 +2,47 @@ var flow = require('../index').flow;
 var parallel = require('../index').parallel;
 
 var myFlow = flow('main')(
-  function one() { this.next(); },
-  function two() { this.next(); },
+  function one() {
+    console.log(this.stepName);
+    this.next(); 
+  },
+  function two() {
+    console.log(this.stepName);
+    this.next(); 
+  },
   parallel('par1')(
     flow('par1-1')(
-      function three() { this.next(); },
-      function four() { this.next(); }
+      function three() {
+        console.log(this.stepName);
+        this.next(); 
+      },
+      function four() {
+        console.log(this.stepName);
+        this.next(); 
+      }
     ),
     flow('par1-2')(
-      function five() { this.next(); },
-      function six() { this.next(); }
+      function five() {
+        console.log(this.stepName);
+        this.next(); 
+      },
+      function six() {
+        console.log(this.stepName);
+        this.next(); 
+      }
     )
   ),
-  function seven() { this.next(); },
-  function eight() { this.next(); },
+  function seven() {
+    console.log(this.stepName);
+    this.next(); 
+  },
+  function eight() {
+    console.log(this.stepName);
+    this.next(); 
+  },
   function allDone() {
     if (this.err) throw this.err;
-    console.log(this.history);
+    console.log(this.stepName);
     this.next();
   }
 );
